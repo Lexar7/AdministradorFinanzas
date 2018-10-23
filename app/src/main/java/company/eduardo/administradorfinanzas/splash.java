0ac085ef9365977fc3f7653db85323a291424d6a
EDUAR7\Eduardo <eduar7nc@gmail.com>
2018-10-23 12:34:52 -0600

    Splash y Login a través de google

---
+++ app/src/main/java/company/eduardo/administradorfinanzas/splash.java
@@ -0,0 +1,38 @@
+package company.eduardo.administradorfinanzas;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    private Boolean botonBackPresionado=false;
+    private static final int DURACION_SPLASF=5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        Handler manejador = new Handler();
+        manejador.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                if (!botonBackPresionado){
+                    Intent intento = new Intent(splash.this, MiPerfilActivity.class);
+                    startActivity(intento);
+                }
+            }
+
+        }, DURACION_SPLASF);
+    }
+
+    public void OnBackPressed(){
+        botonBackPresionado=true;
+        super.onBackPressed();
+    }
+}
