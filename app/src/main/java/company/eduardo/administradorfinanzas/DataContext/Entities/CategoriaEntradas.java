9cca118b69053b3f940b75cb0068915f0963abf5
carol <carolina.rodriguez@catolica.edu.sv>
2018-11-06 00:39:04 -0600

    modelos BD

--- app/src/main/java/company/eduardo/administradorfinanzas/DataContext/Entities/CategoriaEntradas.java
+++ app/src/main/java/company/eduardo/administradorfinanzas/DataContext/Entities/CategoriaEntradas.java
@@ -4,8 +4,6 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
-import android.support.annotation.NonNull;
-
 
 @Entity(tableName = "CategoriaEntradas")
 public class CategoriaEntradas {
