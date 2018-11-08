55ccc9b96d136a93901feb4241ad29d35a4ab250
carol <carolina.rodriguez@catolica.edu.sv>
2018-11-08 00:22:58 -0600

    Creacion Categorias Salidasy Entradas

---
+++ app/src/main/java/company/eduardo/administradorfinanzas/CrearCategoriaEntradas.java
@@ -0,0 +1,68 @@
+package company.eduardo.administradorfinanzas;
+
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import company.eduardo.administradorfinanzas.DataContext.Entities.CategoriaEntradas;
+import company.eduardo.administradorfinanzas.DataContext.Repositories.CategoriaEntradasRepository;
+public class CrearCategoriaEntradas extends AppCompatActivity {
+    private EditText etCategoria;
+    private Button btGuardar, btMostrar;
+    private TextView etMostrar;
+    private CategoriaEntradas mCategoriasEntradas;
+    private CategoriaEntradasRepository categoriaEntradasRepository;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_categoria_entradas);
+        categoriaEntradasRepository = new CategoriaEntradasRepository(getApplication());
+        etCategoria = findViewById(R.id.txtCrearCate);
+        etMostrar= findViewById(R.id.txtMostrar);
+        btGuardar = findViewById(R.id.btnCrear);
+        btMostrar = findViewById(R.id.btnMostrar);
+
+        btGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crear();
+            }
+        });
+
+        btMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar();
+            }
+        });
+
+        categoriaEntradasRepository.getAll().observe(this, new Observer<List<CategoriaEntradas>>() {
+            @Override
+            public void onChanged(@Nullable List<CategoriaEntradas> notes) {
+                for(CategoriaEntradas note : notes) {
+                    System.out.println("-----------------------");
+                    System.out.println(note.getIdCategoria());
+                    System.out.println(note.getName());
+                    System.out.println(note.getImage());
+                }
+            }
+        });
+
+    }
+    private void crear() {
+        String textoCategoria = etCategoria.getText().toString();
+        mCategoriasEntradas = new CategoriaEntradas(textoCategoria, "@drawable/money.png");
+        categoriaEntradasRepository.insert(mCategoriasEntradas);
+
+    }
+    private void mostrar(){
+
+    }
+}
---
+++ app/src/main/java/company/eduardo/administradorfinanzas/CrearCategoriasSalidas.java
@@ -0,0 +1,70 @@
+package company.eduardo.administradorfinanzas;
+
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import company.eduardo.administradorfinanzas.DataContext.Entities.CategoriaEntradas;
+import company.eduardo.administradorfinanzas.DataContext.Entities.CategoriaSalidas;
+import company.eduardo.administradorfinanzas.DataContext.Repositories.CategoriaEntradasRepository;
+import company.eduardo.administradorfinanzas.DataContext.Repositories.CategoriaSalidasRepository;
+
+public class CrearCategoriasSalidas extends AppCompatActivity {
+    private EditText etCategoria;
+    private Button btGuardar, btMostrar;
+    private TextView etMostrar;
+    private CategoriaSalidas mCategoriasSalidas;
+    private CategoriaSalidasRepository categoriaSalidasRepository;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_categorias_salidas);
+        categoriaSalidasRepository = new CategoriaSalidasRepository(getApplication());
+        etCategoria = findViewById(R.id.txtCrearCate);
+        etMostrar= findViewById(R.id.txtMostrar);
+        btGuardar = findViewById(R.id.btnCrear);
+        btMostrar = findViewById(R.id.btnMostrar);
+
+        btGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crear();
+            }
+        });
+
+        btMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar();
+            }
+        });
+//Verificar si  insertó en la BD
+        categoriaSalidasRepository.getAll().observe(this, new Observer<List<CategoriaSalidas>>() {
+            @Override
+            public void onChanged(@Nullable List<CategoriaSalidas> notes) {
+                for(CategoriaSalidas note : notes) {
+                    System.out.println("-----------------------");
+                    System.out.println(note.getIdCategoria());
+                    System.out.println(note.getName());
+                    System.out.println(note.getImage());
+                }
+            }
+        });
+    }
+    private void crear() {
+        String textoCategoria = etCategoria.getText().toString();
+        mCategoriasSalidas = new CategoriaSalidas(textoCategoria, "@drawable/money.png");
+        categoriaSalidasRepository.insert(mCategoriasSalidas);
+
+    }
+    private void mostrar(){
+
+    }
+}
