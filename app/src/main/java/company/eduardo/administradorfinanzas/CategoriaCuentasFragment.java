b47ed9105d80470ebe030cc4d45d485f7367d1d6
Eduardo Noyola <eduar7nc@gmail.com>
2018-11-12 00:05:10 -0600

    Direccionamiento de botones categorias

--- app/src/main/java/company/eduardo/administradorfinanzas/CategoriaCuentasFragment.java
+++ app/src/main/java/company/eduardo/administradorfinanzas/CategoriaCuentasFragment.java
@@ -1,5 +1,6 @@
 package company.eduardo.administradorfinanzas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,6 +19,8 @@ public class CategoriaCuentasFragment extends Fragment {
     View view;
 
     private ListView lv1;
+
+    Button btnNew;
 
     public String categorias [] = {"Salarios", "Publicidad", "Compras", "Arrendamientos", "Canones", "Reparaciones",
     "Auditorias", "Servicios Independientes", "Suministros", "Tributos", "Gastos Financieros", "Otros Gastos"};
@@ -31,9 +35,19 @@ public class CategoriaCuentasFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_categoria_cuentas, container, false);
 
+        btnNew = (Button)view.findViewById(R.id.btnNuevo);
+
         lv1 = (ListView)view.findViewById(R.id.lv);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_categorias, categorias);
         lv1.setAdapter(adapter);
+
+        btnNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), CrearCategorias.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
--- app/src/main/java/company/eduardo/administradorfinanzas/CategoriaSalidasFragment.java
+++ app/src/main/java/company/eduardo/administradorfinanzas/CategoriaSalidasFragment.java
@@ -1,5 +1,6 @@
 package company.eduardo.administradorfinanzas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class CategoriaSalidasFragment extends Fragment {
 
     View view;
+
+    Button btnNew;
 
     private ListView lv1;
 
@@ -30,9 +34,19 @@ public class CategoriaSalidasFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_categoria_salidas, container, false);
 
+        btnNew = (Button)view.findViewById(R.id.btnNuevo);
+
         lv1 = (ListView)view.findViewById(R.id.lv);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_categorias, categorias);
         lv1.setAdapter(adapter);
+
+        btnNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), CrearCategoriasSalidas.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
